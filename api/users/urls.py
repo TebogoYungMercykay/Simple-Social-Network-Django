1f7cd9ada86ca91eaafa1357b9a926a22347e4ca
tebogoyungmercykay <sbkskhalo.kq@gmail.com>
2025-09-27 10:00:52 +0200

    (py) User-specific URL Routing

---
+++ api/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('register/', views.register_view, name='register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('verify-email/<uuid:token>/', views.verify_email, name='verify_email'),
+    path('resend-verification/<int:user_id>/', views.resend_verification_email, name='resend_verification'),
+]
